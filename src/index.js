168a474945c9e798d60036aaca0d5022d953a024
yshir <yshir0020@gmail.com>
2020-04-07 03:12:57 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 exports.handler = async (event, context) => {
-  console.log('updated!')
   console.log(JSON.stringify(event))
-
   context.succeed({
     statusCode: 200,
     body: { success: true }
